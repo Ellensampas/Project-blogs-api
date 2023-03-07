9ed14acffc7c1598a40531cd39a95faebcf59825
ellen Sampaio <ellenssbarross@gmail.com>
2023-03-07 14:56:19 -0300

    req07 - concluido

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,20 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+  const Category = sequelize.define('Category', {
     id: {
       primaryKey: true,
       allowNull: false,
       autoIncrement: true,
       type: DataTypes.INTEGER
     },
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    image: DataTypes.STRING,
+    name: DataTypes.STRING,
   },
     {
-    tableName: 'users',
+    tableName: 'categories',
     underscored: true,
     timestamps: false,
     });
-  return User;
+
+  return Category;
 };
